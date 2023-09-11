199955c8992c23bc721ce09d4d164df66f231443
boczvlogs <144637954+boczvlogs@users.noreply.github.com>
2023-09-11 09:12:38 +0800

    Create prefinals.py
    Pre Finals

---
+++ prefinals.py
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<fstream>
+using namespace std;
+
+  int addition (int x, int y)
+{
+ int a;
+ifstream boczx("input.txt");
+ while(boczx>>x>>y)
+{
+a = x + y;
+ return (a);
+    }
+}
+main ()
+{
+ int x,y;
+	ifstream boczx("input.txt");
+	while(boczx>>x>>y)
+{
+ int sum = x+y;
+ cout <<sum<<endl;
+ }
+                            
+	system("pause");
+}
