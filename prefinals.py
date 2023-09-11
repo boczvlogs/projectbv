0f27d2a6688bfeaae7f7e970d626a28c305094e1
Bocz Vlogs <144637954+boczvlogs@users.noreply.github.com>
2023-09-11 13:52:32 +0800

    Update prefinals.py

--- prefinals.py
+++ prefinals.py
@@ -15,8 +15,8 @@ a = x + y;
 main ()
 {
  int x,y;
-     ifstream boczx("input.txt");
-     while(boczx>>x>>y)
+   ifstream boczx("input.txt");
+   while(boczx>>x>>y)
 {
  int sum = x+y;
  cout <<sum<<endl;
